885e7246fefaa07362e27fd1eac6e11887a7a218
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-08-31 02:22:44 +0400

    Added Wikipedia Analysis ✅

--- app/main.py
+++ app/main.py
@@ -106,9 +106,24 @@ def news():
     # Print the layout to display the news panel
     return news_panel
 
+def get_wikipedia_summary(topic):
+    base_url = "https://en.wikipedia.org/api/rest_v1/page/summary/"
+    full_url = base_url + topic
+    response = requests.get(full_url)
+    data = response.json()
+    summary = data.get("extract", "No summary available.")
+    return summary
+
+def wikipedia_main():
+    political_figure = "Barack_Obama"
+    topic_summary = get_wikipedia_summary(political_figure)
     
+    return Panel(topic_summary, title="Entity profilling", border_style="bold white", box=box.SQUARE)
+
+
 layout["Header"].update(Header())
 layout["Footer"].update(Footer())
 layout["News"].update(news())
+layout["Entity Profilling"].update(wikipedia_main())
 
 print(layout)
