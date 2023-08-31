a3af03cfc755c52993d43192525d96c5ced41c76
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-09-01 02:21:00 +0400

    Added base code for data integration from WBGAPI ✅

--- tools/WorldBankAPI.py
+++ tools/WorldBankAPI.py
@@ -4,15 +4,5 @@ from rich.traceback import install
 install(show_locals=True)
 
 # Search for inflation-related series information
-data = wb.series.info(q="inflation")
-
-# Extract numeric data from the response
-numeric_data = {}
-for entry in data:
-    series_id = entry['id']
-    series_name = entry['value']
-    numeric_data[series_name] = wb.data.DataFrame(series_id, time="YR")
-
-# Print the numeric data
-for series_name, df in numeric_data.items():
-    print(f"Numeric data for {series_name}:\n{df}\n")
+data = wb.data.DataFrame("FP.CPI.TOTL.ZG", "USA").transpose()
+print(data)
