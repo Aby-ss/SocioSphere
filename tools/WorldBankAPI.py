ee32a3530ceefa51753cfe00173939c751cee00c
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-09-01 02:15:37 +0400

    Added API tool file for World Bank Gov API ✅

---
+++ tools/WorldBankAPI.py
@@ -0,0 +1,15 @@
+import wbgapi as wb
+
+# Search for inflation-related series information
+data = wb.series.info(q="inflation")
+
+# Extract numeric data from the response
+numeric_data = {}
+for entry in data:
+    series_id = entry['id']
+    series_name = entry['value']
+    numeric_data[series_name] = wb.data.DataFrame(series_id, time="YR")
+
+# Print the numeric data
+for series_name, df in numeric_data.items():
+    print(f"Numeric data for {series_name}:\n{df}\n")
