c32f36af41c97071b43692fc45db02a3660e5092
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-09-01 02:40:19 +0400

    Print Value ID data ✅

--- tools/WorldBankAPI.py
+++ tools/WorldBankAPI.py
@@ -3,6 +3,16 @@ import wbgapi as wb
 from rich.traceback import install
 install(show_locals=True)
 
-# Search for inflation-related series information
-data = wb.data.DataFrame("FP.CPI.TOTL.ZG", "USA").transpose()
-print(data)
+inflation_data = wb.series.info(q="inflation")
+expenses_data = wb.series.info(q="expenses")
+gdp_data = wb.series.info(q="gdp")
+gdpGrowth_data = wb.series.info(q="gdp growth")
+grossSaving_data = wb.series.info(q="gross saving")
+exports_data = wb.series.info(q="exports of goods and services")
+
+print(inflation_data)
+print(expenses_data)
+print(exports_data)
+print(gdp_data)
+print(gdpGrowth_data)
+print(grossSaving_data)
