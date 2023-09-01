e9b18ec6632091e3c65818668ffbb0e7ca4b8f78
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-09-02 02:17:08 +0400

    Fast Code optimisation ✅

--- tools/WorldBankAPI.py
+++ tools/WorldBankAPI.py
@@ -3,30 +3,12 @@ import wbgapi as wb
 from rich.traceback import install
 install(show_locals=True)
 
-inflation_data = wb.series.info(q="inflation")
-expenses_data = wb.series.info(q="expenses")
-gdp_data = wb.series.info(q="gdp")
-gdpGrowth_data = wb.series.info(q="gdp growth")
-grossSaving_data = wb.series.info(q="gross saving")
-exports_data = wb.series.info(q="exports")
-
-print(inflation_data)
-print(expenses_data)
-print(exports_data)
-print(gdp_data)
-print(gdpGrowth_data)
-print(grossSaving_data)
-
-print(" ")
-print("-" * 150)
-print(" ")
-
-inflation_data = wb.data.DataFrame("FP.CPI.TOTL.ZG").transpose()
-expenses_data = wb.data.DataFrame("NE.EXP.GNFS.CD").transpose()
-gdp_data = wb.data.DataFrame("NY.GDP.MKTP.CD").transpose()
-gdpGrowth_data = wb.data.DataFrame("NY.GDP.MKTP.KD.ZG").transpose()
-grossSaving_data = wb.data.DataFrame("NY.GNS.ICTR.CD").transpose()
-exports_data = wb.data.DataFrame("NE.EXP.GNFS.CD").transpose()
+inflation_data = wb.data.DataFrame("FP.CPI.TOTL.ZG", "USA").transpose()
+expenses_data = wb.data.DataFrame("NE.EXP.GNFS.CD", "USA").transpose()
+gdp_data = wb.data.DataFrame("NY.GDP.MKTP.CD", "USA").transpose()
+gdpGrowth_data = wb.data.DataFrame("NY.GDP.MKTP.KD.ZG", "USA").transpose()
+grossSaving_data = wb.data.DataFrame("NY.GNS.ICTR.CD", "USA").transpose()
+exports_data = wb.data.DataFrame("NE.EXP.GNFS.CD", "USA").transpose()
 
 print(inflation_data)
 print(expenses_data)
