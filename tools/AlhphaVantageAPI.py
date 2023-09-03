4b4c019b17f1e512cfc29c45a1bd7d2b69de6ba0
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-09-03 14:18:54 +0400

    Added multiple company data integration ✅

--- tools/AlhphaVantageAPI.py
+++ tools/AlhphaVantageAPI.py
@@ -5,6 +5,7 @@ import asciichartpy
 from rich.traceback import install
 install(show_locals=True)
 
+# List of major corporation codes
 major_corporation_codes = [
     "AAPL",   # Apple Inc.
     "MSFT",   # Microsoft Corporation
@@ -19,32 +20,37 @@ major_corporation_codes = [
     "BLK",    # BlackRock, Inc.
 ]
 
+# Alpha Vantage API configuration
 base_url = 'https://www.alphavantage.co/query'
 function = 'TIME_SERIES_DAILY'
 output_size = 'compact'
 
+# Common chart parameters
 chart_params = {
     'function': function,
-    'symbol': "MSFT",
+    'symbol': "",  # Symbol will be updated in the loop
     'outputsize': output_size,
-    'apikey': "78H5RH2BRNG4G5Z6"
+    'apikey': "YOUR_API_KEY_HERE",  # Replace with your Alpha Vantage API key
 }
 
-        
-response = requests.get(base_url, params=chart_params)
-data = response.json()
+# Iterate through corporation codes and fetch data
+for code in major_corporation_codes:
+    chart_params['symbol'] = code  # Set the symbol to the current corporation code
+    response = requests.get(base_url, params=chart_params)
+    data = response.json()
 
-if 'Error Message' in data:
-    print(f"Error: {data['Error Message']}")
-else:
-    time_series = data['Time Series (Daily)']
-    dates = []
-    close_prices = []
+    if 'Error Message' in data:
+        print(f"Error for {code}: {data['Error Message']}")
+    else:
+        time_series = data['Time Series (Daily)']
+        dates = []
+        close_prices = []
 
-    for date, values in time_series.items():
-        dates.append(date)
-        close_prices.append(float(values['4. close']))
+        for date, values in time_series.items():
+            dates.append(date)
+            close_prices.append(float(values['4. close']))
 
-    # Create and display ASCII chart
-    chart = asciichartpy.plot(close_prices, {"width": 5, "height": 10, "format": "{:8.2f}"})
-    print(chart)
+        # Create and display ASCII chart for the current corporation
+        print(f"Stock Prices for {code}:")
+        chart = asciichartpy.plot(close_prices, {"width": 50, "height": 10, "format": "{:8.2f}"})
+        print(chart)
