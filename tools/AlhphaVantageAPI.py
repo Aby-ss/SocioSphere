307e3e9e807178bfe49f1dadb3d1904efc101a66
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-09-03 14:17:22 +0400

    Added Compny codes for list ✅

--- tools/AlhphaVantageAPI.py
+++ tools/AlhphaVantageAPI.py
@@ -5,6 +5,20 @@ import asciichartpy
 from rich.traceback import install
 install(show_locals=True)
 
+major_corporation_codes = [
+    "AAPL",   # Apple Inc.
+    "MSFT",   # Microsoft Corporation
+    "AMZN",   # Amazon.com Inc.
+    "GOOGL",  # Alphabet Inc. (Google)
+    "FB",     # Facebook, Inc.
+    "TSLA",   # Tesla, Inc.
+    "BRK.B",  # Berkshire Hathaway Inc.
+    "JPM",    # JPMorgan Chase & Co.
+    "JNJ",    # Johnson & Johnson
+    "V",      # Visa Inc.
+    "BLK",    # BlackRock, Inc.
+]
+
 base_url = 'https://www.alphavantage.co/query'
 function = 'TIME_SERIES_DAILY'
 output_size = 'compact'
