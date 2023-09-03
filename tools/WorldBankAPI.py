bfcf0248072fba67236485f303600c564257568c
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-09-03 13:48:24 +0400

    Base code restoration ✅

--- tools/WorldBankAPI.py
+++ tools/WorldBankAPI.py
@@ -1,53 +1,17 @@
 import wbgapi as wb
-import pandas as pd
-import matplotlib.pyplot as plt
-
 from rich.traceback import install
 install(show_locals=True)
 
-# Define the indicators and country code
-indicators = [
-    "FP.CPI.TOTL.ZG",  # Inflation Rate
-    "NE.EXP.GNFS.CD",  # Expenses
-    "NY.GDP.MKTP.CD",  # GDP
-    "NY.GDP.MKTP.KD.ZG",  # GDP Growth Rate
-    "NY.GNS.ICTR.CD",  # Gross Savings
-    "NE.EXP.GNFS.CD"  # Exports
-]
-country_code = "USA"
+inflation_data = wb.data.DataFrame("FP.CPI.TOTL.ZG", "USA").transpose()
+expenses_data = wb.data.DataFrame("NE.EXP.GNFS.CD", "USA").transpose()
+gdp_data = wb.data.DataFrame("NY.GDP.MKTP.CD", "USA").transpose()
+gdpGrowth_data = wb.data.DataFrame("NY.GDP.MKTP.KD.ZG", "USA").transpose()
+grossSaving_data = wb.data.DataFrame("NY.GNS.ICTR.CD", "USA").transpose()
+exports_data = wb.data.DataFrame("NE.EXP.GNFS.CD", "USA").transpose()
 
-# Initialize an empty list to store DataFrames
-data_frames = []
-
-# Fetch data for each indicator and store it in a DataFrame
-for indicator in indicators:
-    try:
-        data = wb.data.DataFrame(indicator, country_code).transpose()
-        data_frames.append(data)
-    except KeyError:
-        print(f"Data not available for indicator: {indicator}")
-
-# Concatenate the DataFrames horizontally using the year as the index
-combined_data = pd.concat(data_frames, axis=1)
-
-# Print the combined data as a table
-print(combined_data)
-
-# Turn on interactive mode for Matplotlib
-plt.ion()
-
-# Create a function to plot and display data using Matplotlib
-def plot_data(indicator_data, indicator_name):
-    plt.figure(figsize=(10, 6))
-    plt.plot(indicator_data.index, indicator_data.values, marker='o', linestyle='-')
-    plt.title(f"{indicator_name} Over Time")
-    plt.xlabel("Year")
-    plt.ylabel(indicator_name)
-    plt.grid(True)
-    plt.show()
-
-# Create and display line plots for each indicator using Matplotlib
-for indicator in indicators:
-    if indicator in combined_data.columns:
-        indicator_data = combined_data[indicator]
-        plot_data(indicator_data, indicator)
+print(inflation_data)
+print(expenses_data)
+print(exports_data)
+print(gdp_data)
+print(gdpGrowth_data)
+print(grossSaving_data)
